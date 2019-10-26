88d3ff9a3a6d5ee55f280f8c95dde205153994d1
Roshan Kumaraswamy <roshie548@gmail.com>
2019-10-26 01:42:14 -0700

    started working on listings

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -10,4 +10,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+//    TODO: an Adapter class and create an instance of it
+//    https://developer.android.com/reference/android/widget/ListView
+//    https://developer.android.com/guide/topics/ui/declaring-layout.html#FillingTheLayout
+
+//    Listing
+    class Listing {
+        String name;
+        String address;
+        double price;
+
+        Listing(String name, String address, double price) {
+            this.name = name;
+            this.address = address;
+        }
+    }
 }
